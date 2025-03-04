e81cc6f0d3602a8cae0601a85e9aca94c95cda58
recep-demir <demir.rp@gmail.com>
2025-03-04 22:56:50 +0100

    edit store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,8 +1,9 @@
 
 import { configureStore } from '@reduxjs/toolkit';
+import productSlice from "../features/productSlice"
 
 export const store = configureStore({
   reducer: {
-    example: exampleReducer,
+    product: productSlice,
   },
 });
