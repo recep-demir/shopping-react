530cc9303150d7d47035e8d9a7db47c8ef57a60f
recep-demir <demir.rp@gmail.com>
2025-03-05 01:09:26 +0100

    edit navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,17 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import StoreIcon from '@mui/icons-material/Store';
+import { Link } from 'react-router';
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{backgroundColor:"white", color:"black"}}>
         <Toolbar>
+          <Link to="/" style={{ color: 'inherit', textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
           <IconButton
             size="large"
             edge="start"
@@ -19,12 +21,28 @@ export default function ButtonAppBar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <StoreIcon fontSize="large" />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          </Link>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            Shopping
           </Typography>
-          <Button color="inherit">Login</Button>
+
+          <Link to="basket" style={{ color: 'inherit', textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
+
+          
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 3 }}
+          >
+            <ShoppingCartIcon fontSize="large" />
+          </IconButton>
+          </Link>
+
+          
         </Toolbar>
       </AppBar>
     </Box>
