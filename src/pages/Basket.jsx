965a397842082379a54cc14d1b00e9534cd2842e
recep-demir <demir.rp@gmail.com>
2025-03-06 12:32:21 +0100

    refactor: eorganize component structure

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,7 +1,39 @@
-
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import BasketCard from '../components/BasketCard'
 
 const Basket = () => {
-  return <div>Basket</div>;
-};
+  const {basket} =useSelector((state)=>state.basketSlice)
+  return (
+    <Box>
+      <Typography textAlign="center" p={2} m={2} variant='h3'>
+        My Basket
+      </Typography>
+      <Box sx={{display:"flex", flexWrap:"wrap", justifyContent:"center"}}>
+        {basket.length>0 ? (
+          basket.map((item)=>(
+            <Box key={item.id} sx ={{position:"relative"}}>
+              <BasketCard {...item}/>
+            </Box>
+          ))
 
-export default Basket;
+        ):(
+          <Typography
+          sx={{textAlign:"center"}}>
+            You have no items in your shopping cart, start adding some!
+          </Typography>
+
+        )}
+
+
+
+
+      </Box>
+      
+
+    </Box>
+  )
+}
+
+export default Basket
