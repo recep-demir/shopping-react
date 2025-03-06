0fe31dbc1c6334854635d3c3e18e8931ee911f34
recep-demir <demir.rp@gmail.com>
2025-03-06 01:37:32 +0100

    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import { store } from "./app/store";
 import AppRouter from "./router/AppRouter";
-
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-  <div>
-    <Provider store ={store}>
-   <AppRouter/>
-   </Provider>
-
-
-  </div>
-  
-  
-  )
+    <div>
+      <Provider store={store}>
+        <AppRouter />
+        <ToastContainer />
+      </Provider>
+    </div>
+  );
 }
 
 export default App;
